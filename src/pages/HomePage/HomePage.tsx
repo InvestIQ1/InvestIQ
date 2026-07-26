8f07cbb234340ec0393d780a7d1c7170f76c5d8f
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-07-26 14:39:48 +0300

    Clean up HomePage by removing unused components
    Removed Header, PeriodDate, and ChooseCategory components from HomePage.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,7 @@
-import { Header } from "../../components/header/Header";
-import { ChooseCategory } from "../../components/chooseCategory/ChooseCategory";
-import { PeriodDate } from "../../components/periodDate/PeriodDate";
 
 
 export default function HomePage () {
     return (
-        <div>
-            <Header/>
-            <PeriodDate/>
-            <ChooseCategory/>
-        </div>
+        <div></div>
     )
 }
