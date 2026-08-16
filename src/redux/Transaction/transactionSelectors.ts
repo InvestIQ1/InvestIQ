b077f7506525b1e0a59b339c71092346e7c028b0
artem1214 <61391756+artemanoh@users.noreply.github.com>
2026-08-16 18:02:37 +0300

    make js balance

--- src/redux/Transaction/transactionSelectors.ts
+++ src/redux/Transaction/transactionSelectors.ts
@@ -7,3 +7,13 @@ export const selectLoading = (state: RootState) => state.transaction.loading;
 
 export const selectBalance = (state: RootState) =>
   state.transaction.transactions.reduce((acc, t) => acc + t.sum, 0);
+
+export const selectIncome = (state: RootState) =>
+  state.transaction.transactions
+    .filter((t) => t.type === "income")
+    .reduce((acc, t) => acc + t.sum, 0);
+
+export const selectExpense = (state: RootState) =>
+  state.transaction.transactions
+    .filter((t) => t.type === "expense")
+    .reduce((acc, t) => acc + t.sum, 0);
